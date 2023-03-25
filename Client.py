341595f5e70f3a0cadf59e2c6038cf63a115ecd6
Aurelius Lim <80933291+AureliusLim@users.noreply.github.com>
2023-03-25 12:42:12 +0800

    connect, and register alias

--- Client.py
+++ Client.py
@@ -17,3 +17,67 @@ error_command = {"command": "error", "message": "message"}
 client_socket= socket.socket(family=socket.AF_INET, type = socket.SOCK_DGRAM)
 client_socket.settimeout(1)
 
+def get_from_server():
+    pass
+
+def main():
+    connected =  False
+    registered = False
+    while True:
+        while connected == False: # not yet connected to server
+            client_input = input()
+            command = client_input[1:] # remove slash
+            
+            if command == "?": # help command
+                 print("COMMAND LIST:")
+                 print("Connect to the server application: /join <server_ip_add> <port>\nDisconnect to the server application: /leave\nRegister a unique handle or alias: /register <handle>\nSend message to all: /all <message>\nSend direct message to a single handle: /msg <handle> <message>\nRequest command help to output all Input Syntax commands for references: /?")
+                 continue
+           
+            firstword = command.split(" ", 1)
+
+            try:
+                if firstword[0] == "join":
+                    
+                        fullcommand = command.split(" ")
+                        server_IP= fullcommand[1]
+                        PORT_NO = fullcommand[2]
+                            
+                        client_socket.sendto(bytes(json.dumps(join_command), "utf-8"), ( server_IP, int(PORT_NO)))
+                        data, server = client_socket.recvfrom(1024)
+                        data = data.decode("utf-8")
+                        json_data = json.loads(data)
+                        connected = True
+                        print("Connection to the Message Board Server is successful!")
+            except:
+                print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+            
+        while connected == True:
+            multi_thread = threading.Thread(target=get_from_server, args=(()))
+            multi_thread.start() 
+            input_on_chat = input()
+            line = input_on_chat[1:]#remove slash
+            #leave 
+            if line == "leave":
+                if registered == True:
+                    client_socket.sendto(bytes(json.dumps(leave_command), "utf-8"), (server_IP,int(PORT_NO)))
+                    time.sleep(0.2)
+              
+                else:
+                    print("Connection closed. Thank you!")
+                    connected = False
+                continue
+
+            firstline = line.split(" ", 1)
+            if firstline[0] == "register":
+                  alias = firstline[1]
+                  register_command["handle"] = alias
+                  client_socket.sendto(bytes(json.dumps(register_command), "utf-8"),(server_IP,int(PORT_NO)))
+        client_socket.close()
+              
+            
+                        
+                    
+               
+
+if __name__ == '__main__':
+    main()
--- Server.py
+++ Server.py
@@ -13,7 +13,50 @@ server_socket.bind((IP_ADDRESS, PORT_NO))
 
 #continue listening
 
+#setup client connections through dictionary format {"client_address":"alias"}
+client_conns = {}
+
 while(True):
     byte_address = server_socket.recvfrom(bufferSize)
     received_data = byte_address[0]
+    received_client = byte_address[1]
     received_json = json.loads(received_data.decode("utf-8"))
+    print("Received command ", received_data.decode('utf-8'), "from client \'", received_client, "\'")
+
+    #process the received json
+
+    #if it contains the join
+
+    if received_json["command"] == "join":
+        if received_client[0] == IP_ADDRESS and received_client[1] == PORT_NO:
+            send_json = bytes(json.dumps({"command": "join"}), "utf-8")
+            server_socket.sendto(send_json, received_client)
+        else:
+            send_json = bytes(json.dumps({"command":"error", "message":"Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number."}), "utf-8")
+            server_socket.sendto(send_json, received_client)
+
+    # if it contains the leave
+    elif received_json["command"] == "leave":
+        #remove registered user from the dict of clients
+        username = client_conns[received_client]
+        print("User ", username, " left the message board")
+        del client_conns[received_client]
+        command = bytes(json.dumps({"command": "leave"}), "utf-8")
+        server_socket.sendto(command, received_client)
+    
+    #if it contains the register
+
+    elif received_json["command"] == "register":
+       
+        
+        alias = received_json["handle"]
+         #double check if handle is already taken
+        client_alias = client_conns.values()
+        if alias in client_alias:
+             command = bytes(json.dumps({"command":"error", "message":"Error: Registration failed. Handle or alias already exists."}), "utf-8")
+             print("The handle ", alias, " is already in use")
+        else:
+            client_conns[received_client] = alias
+            #print(client_conns) #testing
+            command = bytes(json.dumps({"command": "register", "handle": client_conns[received_client]}), "utf-8")
+            server_socket.sendto(command, received_client)
